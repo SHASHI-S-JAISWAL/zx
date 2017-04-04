bf3f9696a0d5fd8b1dcb406c82cf74ae9b48c774
SHASHI S JAISWAL <ssekharj11@gmail.com>
2017-04-04 12:37:43 +0530

    added

---
+++ j1.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test page</title>
+<script >
+	var x={Bid:12,BName:"lets C",Bprice:300};
+
+	document.write(x.Bid +"<br>" );
+	document.write(x.BName +"<br>");
+	document.write(x.Bprice+"<br>");
+
+</script>
+</head>
+<body>
+
+</body>
+</html>>
